9025ff51df815f942349a38847d4e01ce680ca93
Esteban Blanco <estebanbn@gmail.com>
2023-02-16 21:07:58 -0500

    Updated obj file reader

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
 	bool steep = false; 
-	if (std::abs(x0-x1)<std::abs(y0-y1)) { 
+	if (std::abs(x0-x1) < std::abs(y0-y1)) { 
 		std::swap(x0, y0); 
 		std::swap(x1, y1); 
 		steep = true; 
 	} 
-	if (x0>x1) { 
+	if (x0 > x1) { 
 		std::swap(x0, x1); 
 		std::swap(y0, y1); 
 	} 
@@ -27,7 +27,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	int derror2 = std::abs(dy)*2; 
 	int error2 = 0; 
 	int y = y0; 
-	for (int x=x0; x<=x1; x++) { 
+	for (int x=x0; x <= x1; x++) { 
 		if (steep) { 
 			image.set(y, x, color); 
 		} else { 
@@ -35,14 +35,14 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 		} 
 		error2 += derror2; 
 		if (error2 > dx) { 
-			y += (y1>y0?1:-1); 
+			y += (y1 > y0 ? 1 : -1); 
 			error2 -= dx*2; 
 		} 
 	} 
 }
 
 int main(int argc, char** argv) {
-	if (2==argc) {
+	if (2 == argc) {
 		model = new Model(argv[1]);
 	} else {
 		model = new Model("obj/african_head.obj");
@@ -55,10 +55,10 @@ int main(int argc, char** argv) {
 		for (int j=0; j < 3; j++) {
 			Vec3f v0 = model->vert(face[j]);
 			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x+1.)*width/2.;
-			int y0 = (v0.y+1.)*height/2.;
-			int x1 = (v1.x+1.)*width/2.;
-			int y1 = (v1.y+1.)*height/2.;
+			int x0 = (v0.x + 1.) * width/2.;
+			int y0 = (v0.y + 1.) * height/2.;
+			int x1 = (v1.x + 1.) * width/2.;
+			int y1 = (v1.y + 1.) * height/2.;
 			line(x0, y0, x1, y1, image, white);
 		}
 	}
