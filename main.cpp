b2bf2bfde23f90a9f00b72302db89f725d396fb4
Esteban Blanco <estebanbn@gmail.com>
2023-02-16 23:10:18 -0500

    Clean code and triangle calls

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#include <shlwapi.h>
+
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green   = TGAColor(0, 255,   0,   255);
 Model *model = NULL;
 const int width  = 800;
 const int height = 800;
@@ -41,15 +44,27 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	} 
 }
 
-int main(int argc, char** argv) {
-	if (2 == argc) {
-		model = new Model(argv[1]);
-	} else {
-		model = new Model("obj/african_head.obj");
-	}
-	
-	TGAImage image(width, height, TGAImage::RGB);
-	
+void drawCenteredLine(Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color) {
+	// line((v1.x + 1.) * width / 2., (v1.y + 1.) * height/2., (v2.x + 1.) * width / 2., (v2.y + 1.) * height / 2., image, color); 
+	std::cout << (((100 - (180*100/width)) / 100) * width) << "\n";
+	std::cout << (100. - (180.*100./width)) / 100. << "\n";
+	line(
+		v1.x + (((100. - (180.*100./width)) / 100.) * width),
+		v1.y + (((100. - (180.*100./height)) / 100.) * height),
+		v2.x + (((100. - (180.*100./width)) / 100.) * width),
+		v2.y + (((100. - (180.*100./height)) / 100.) * height),
+		image,
+		color
+	); 
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
+	drawCenteredLine(t0, t1, image, color); 
+	drawCenteredLine(t1, t2, image, color); 
+	drawCenteredLine(t2, t0, image, color); 
+}
+
+void drawObjModel(TGAImage &image) {
 	for (int i=0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
 		for (int j=0; j < 3; j++) {
@@ -62,10 +77,51 @@ int main(int argc, char** argv) {
 			line(x0, y0, x1, y1, image, white);
 		}
 	}
+}
+
+void drawTriangles(TGAImage &image) {
+	Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)}; 
+	Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)}; 
+	Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)}; 
+	triangle(t0[0], t0[1], t0[2], image, red); 
+	triangle(t1[0], t1[1], t1[2], image, white); 
+	triangle(t2[0], t2[1], t2[2], image, green);
+}
+
+void openTGAOutput() {
+	SHELLEXECUTEINFOW ShExecInfo = {};
+	ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFOW);
+	ShExecInfo.lpVerb = L"edit";
+	ShExecInfo.lpFile = L"D:/_DEV/tinyrenderer/simplerenderer/output.tga";
+	ShExecInfo.nShow = SW_MAXIMIZE;
+	ShExecInfo.fMask = SEE_MASK_NOASYNC;
+
+	ShellExecuteEx(&ShExecInfo);
+
+	if (!ShellExecuteExW(&ShExecInfo)) {
+		// Error reported in GetLastError()
+	}
+}
+
+int main(int argc, char** argv) {
+	if (2 == argc) {
+		model = new Model(argv[1]);
+	} else {
+		model = new Model("obj/african_head.obj");
+	}
+	
+	TGAImage image(width, height, TGAImage::RGB);
+	
+	
+	drawTriangles(image);
+	
 	
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	delete model;
+
+	openTGAOutput();
+	
 	return 0;
 }
 
