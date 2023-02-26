f7562e6b41d93f88a547f7c1631d5f014f2b35a7
Esteban Blanco <estebanbn@gmail.com>
2023-02-26 16:08:02 -0500

    Changed obj model's original file name

--- main.cpp
+++ main.cpp
@@ -90,9 +90,6 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
 	if (t0.y>t1.y) std::swap(t0, t1); 
 	if (t0.y>t2.y) std::swap(t0, t2); 
 	if (t1.y>t2.y) std::swap(t1, t2);
-	
-	drawLine(t0, t1 - t0, image, blue);
-	drawLine(t0, t2 - t0, image, purple);
 
 	int total_height = t2.y-t0.y; 
 	for (int y=t0.y; y<=t1.y; y++) { 
@@ -105,14 +102,21 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
 		for (int j=A.x; j<=B.x; j++) { 
 			image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
 		} 
+	}
+	for (int y=t1.y; y<=t2.y; y++) { 
+		int segment_height = t2.y-t1.y+1; 
+		float alpha = (float)(y-t0.y)/total_height; 
+		float beta  = (float)(y-t1.y)/segment_height; // be careful with divisions by zero 
+		Vec2i A = t0 + (t2-t0)*alpha; 
+		Vec2i B = t1 + (t2-t1)*beta; 
+		if (A.x>B.x) std::swap(A, B); 
+		for (int j=A.x; j<=B.x; j++) { 
+			image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+		} 
 	} 
 }
 
 void drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
-	// std::vector<Vec2f> line1Points = drawLine(t0, t1, image, color); 
-	// std::vector<Vec2f> line2Points = drawLine(t1, t2, image, color); 
-	// std::vector<Vec2f> line3Points = drawLine(t2, t0, image, color);
-	
 	triangle(t0, t1, t2, image, color);
 }
 
